f14e2c70453e1e940921790cc619c84529109598
Elžbieta <>
2021-07-04 15:38:22 +0300

    Serialize arrays.

--- JsonSerializer/JsonSerializer.cs
+++ JsonSerializer/JsonSerializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Globalization;
 using System.Text;
 
@@ -17,22 +18,39 @@ namespace JsonSerializer
                 if (i != 0) sb.Append(",");
                 var value = properties[i].GetValue(o, null);
                 var name = properties[i].Name;
-                switch (properties[i].PropertyType.Name)
-                {
-                    case "String":
-                        sb.Append($"\"{name}\":\"{value}\"");
-                        break;
-                    case "Boolean":
-                        sb.Append($"\"{name}\":{(value != null && (bool) value ? "true" : "false")}");
-                        break;
-                    default:
-                        sb.Append($"\"{name}\":{Convert.ToString(value, CultureInfo.InvariantCulture)}");
-                        break;
-                }
+
+                sb.Append($"\"{name}\":");
+                AppendValue(value, properties[i].PropertyType, sb);
             }
 
             sb.Append("}");
             return sb.ToString();
+        }
+
+        private static void AppendValue(object value, Type type, StringBuilder sb)
+        {
+            if (type.IsArray)
+            {
+                sb.Append("[");
+
+                var arr = (IList) value;
+
+                for (var i = 0; i < arr.Count; i++)
+                {
+                    if (i != 0) sb.Append(",");
+                    AppendValue(arr[i], arr[i]?.GetType(), sb);
+                }
+
+                sb.Append("]");
+                return;
+            }
+
+            sb.Append(type.Name switch
+            {
+                "String" => $"\"{value}\"",
+                "Boolean" => value != null && (bool) value ? "true" : "false",
+                _ => Convert.ToString(value, CultureInfo.InvariantCulture)
+            });
         }
     }
 }
--- NUnitTestJsonSerializer/JsonSerializerTest.cs
+++ NUnitTestJsonSerializer/JsonSerializerTest.cs
@@ -66,5 +66,39 @@ namespace NUnitTestJsonSerializer
             var result = _sut.Serialize(new {IsNew = false});
             Assert.AreEqual("{\"IsNew\":false}", result);
         }
+
+        [Test]
+        public void CorrectlySerializeArray()
+        {
+            var result = _sut.Serialize(new {IsNew = new[] {4, 5}});
+            Assert.AreEqual("{\"IsNew\":[4,5]}", result);
+        }
+
+        [Test]
+        public void CorrectlySerializeEmptyArray()
+        {
+            var result = _sut.Serialize(new {IsNew = new int[] { }});
+            Assert.AreEqual("{\"IsNew\":[]}", result);
+        }
+
+        [Test]
+        public void CorrectlySerialize2DArray()
+        {
+            var result = _sut.Serialize(new {IsNew = new[] {new[] {1, 2}, new[] {3, 4}}});
+            Assert.AreEqual("{\"IsNew\":[[1,2],[3,4]]}", result);
+        }
+
+        [Test]
+        public void CorrectlySerializeVariousArrays()
+        {
+            var result = _sut.Serialize(
+                new
+                {
+                    @string = new[] {"just", "string"},
+                    @bool = new[] {false, true},
+                    @float = new[] {1.2f, 1.4f}
+                });
+            Assert.AreEqual("{\"string\":[\"just\",\"string\"],\"bool\":[false,true],\"float\":[1.2,1.4]}", result);
+        }
     }
 }
